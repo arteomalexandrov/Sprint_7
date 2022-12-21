86f06c47704d84c11d632e926ff71995c20d4967
Artem Alexandrov <arteomalexandrov@yandex.ru>
2022-12-21 22:28:53 +0300

    sprint_7 commit

---
+++ src/main/java/api/client/OrdersClient.java
@@ -0,0 +1,45 @@
+package api.client;
+
+import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
+
+import io.qameta.allure.Step;
+
+import java.io.File;
+
+public class OrdersClient {
+
+    private static final String orders = "/api/v1/orders";
+
+    @Step("Get response for create new order")
+    public Response createNewOrderAndCheckResponse() {
+        return given()
+                .multiPart(new File("src/test/resources/newOrder.json"))
+                .when()
+                .post(orders);
+    }
+
+    @Step("Get response for create new order black and gray")
+    public Response createNewOrderBlackAndGray() {
+        return given()
+                .multiPart(new File("src/test/resources/newOrderBlackAndGray.json"))
+                .when()
+                .post(orders);
+    }
+
+    @Step("Get response for create new order not colors")
+    public Response createNewOrderNotColors() {
+        return given()
+                .multiPart(new File("src/test/resources/newOrderNotColors.json"))
+                .when()
+                .post(orders);
+    }
+
+    @Step("Get response for get list orders")
+    public Response getListOrdersAndCheckResponse() {
+        return given()
+                .get(orders);
+    }
+
+}
